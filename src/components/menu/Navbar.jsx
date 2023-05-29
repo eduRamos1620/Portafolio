4cb46a380fe09477e26a5464176bfa0bdea7665f
azazel <eduardoramosrosales16@gmail.com>
2023-05-28 19:39:14 -0600

    Estilo del navbar modificado

--- src/components/menu/Navbar.jsx
+++ src/components/menu/Navbar.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/Navbar.css'
+import '../../components/styles/Navbar.css'
 const Navbar = () => {
   return (
     <div>
-        <nav class="navbar-contenedor">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to='/'>Eduardo Ramos</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav mx-auto">
-                        <Link className="nav-link" to='/'>Inicio</Link>
-                        <Link className="nav-link" to='/pokemon'>Pokemon</Link>
-                        <Link className="nav-link" to='/Items'>Items</Link>
-                        
-                    </div>
-                </div>
-            </div>
+        <nav className="navbar-contenedor">
+            <Link className="name-icon" to='/'>Eduardo Ramos</Link>            
+            <Link className="nav-link" to='/'>Inicio</Link>
+            <Link className="nav-link" to='/Items'>Items</Link>                        
+            <Link className="nav-link" to='/pokemon'>Pokemon</Link>              
         </nav>
     </div>
   )
